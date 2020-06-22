73dae132b2960924bf76d3351d09fb696fe7abf0
Satyajit Sarangi <satyajit@zerodha.com>
2020-06-22 12:22:23 +0530

    Added s3copy to s3

--- s3copy--0.0.1.sql
+++ s3copy--0.0.1.sql
@@ -3,9 +3,8 @@
 
 CREATE SCHEMA IF NOT EXISTS s3copy;
 
-CREATE OR REPLACE FUNCTION s3copy.table_import_from_s3 (
+CREATE OR REPLACE FUNCTION s3copy.import_from_s3 (
    table_name text,
-   column_list text,
    endpoint_url text
 ) RETURNS int
 LANGUAGE plpython3u
@@ -23,42 +22,60 @@ AS $$
             return _module
 
     boto3 = cache_import('boto3')
-    tempfile = cache_import('tempfile')
-    gzip = cache_import('gzip')
-    shutil = cache_import('shutil')
-    urlparse = cache_import('urlparse')
+    urlparse = cache_import('urllib.parse')
 
     plan = plpy.prepare('select current_setting($1, true)::int', ['TEXT'])
 
-    s3 = boto3.client(
-        's3',
-        endpoint_url=endpoint_url,
-    )
+    s3_client = boto3.client('s3')
 
     parsed_s3_url = urlparse.urlparse(endpoint_url)
     bucket = parsed_s3_url.netloc
-    file_path = parsed_s3_url.path
+    file_path = parsed_s3_url.path.lstrip('/')
 
-    response = s3.head_object(Bucket=bucket, Key=file_path)
-    content_encoding = response.get('ContentEncoding')
-
-    with tempfile.NamedTemporaryFile() as fd:
-        if content_encoding and content_encoding.lower() == 'gzip':
-            with tempfile.NamedTemporaryFile() as gzfd:
-                s3.download_fileobj(bucket, file_path, gzfd)
-                gzfd.flush()
-                gzfd.seek(0)
-                shutil.copyfileobj(gzip.GzipFile(fileobj=gzfd, mode='rb'), fd)
-        else:
-                s3.download_fileobj(bucket, file_path, fd)
-        fd.flush()
-        formatted_column_list = "({column_list})".format(column_list=column_list) if column_list else ''
-        res = plpy.execute("COPY {table_name} {formatted_column_list} FROM {filename} {options};".format(
+    response = s3_client.download_file(bucket, file_path, '/tmp/s3_data.csv')
+    res = plpy.execute("COPY {table_name}  FROM {file_name} ;".format(
                 table_name=table_name,
-                filename=plpy.quote_literal(fd.name),
-                formatted_column_list=formatted_column_list,
-                options=options
+                file_name=plpy.quote_literal('/tmp/s3_data.csv')
             )
         )
-        return res.nrows()
+    return res.nrows()
+$$;
+
+CREATE OR REPLACE FUNCTION s3copy.import_to_s3 (
+   query text,
+   file_name text,
+   endpoint_url text
+) RETURNS int
+LANGUAGE plpython3u
+AS $$
+    def cache_import(module_name):
+        module_cache = SD.get('__modules__', {})
+        if module_name in module_cache:
+            return module_cache[module_name]
+        else:
+            import importlib
+            _module = importlib.import_module(module_name)
+            if not module_cache:
+                SD['__modules__'] = module_cache
+            module_cache[module_name] = _module
+            return _module
+
+    boto3 = cache_import('boto3')
+    urlparse = cache_import('urllib.parse')
+
+    plan = plpy.prepare('select current_setting($1, true)::int', ['TEXT'])
+
+    s3_client = boto3.client('s3')
+
+    parsed_s3_url = urlparse.urlparse(endpoint_url)
+    bucket = parsed_s3_url.netloc
+    file_path = parsed_s3_url.path.lstrip('/')
+
+    res = plpy.execute("COPY {query}  to {file_name} ;".format(
+                query=query,
+                file_name=plpy.quote_literal('/tmp/{file_name}'.format(file_name=file_name))
+            )
+        )
+    response = s3_client.upload_file('/tmp/{file_name}'.format(file_name=file_name), bucket, file_path)
+    return res.nrows()
 $$;
